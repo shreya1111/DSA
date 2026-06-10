adbb86cf6c6d62a45c8b4e4a65be6ba488639d5a
Shreya Srivastava <shreyasrivastava739@gmail.com>
2026-06-10 10:45:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.63%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        long start=1;
+        long end=num;
+        while(start<=end){
+            long mid = start + (end - start) / 2;
+
+            long sqrt=mid*mid;
+            if(sqrt==num){
+                return true;
+            }
+            else if(sqrt>num){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return false;
+    }
+}
