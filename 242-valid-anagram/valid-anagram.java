4b242e833a11e434e608ad623131e35252c1c00d
Shreya Srivastava <shreyasrivastava739@gmail.com>
2026-06-10 11:47:25 +0530

    Time: 4 ms (82.25%) | Memory: 46.5 MB (23.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,15 @@
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int i = 0;
-        if(s.length()!=(t.length())){
+        if (s.length() != t.length()) {
             return false;
-           
         }
 
-        while (i < s.length()) {
-            char ch = s.charAt(i);
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
 
-            int index=t.indexOf(ch);
-            if(index==-1){
-                return false;
-            }
-            t=t.substring(0,index)+t.substring(index+1);
-            i++;
-        }
+        Arrays.sort(a);
+        Arrays.sort(b);
 
-        return true;
+        return Arrays.equals(a, b);
     }
 }
-
