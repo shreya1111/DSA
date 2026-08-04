9e0074645d44cc71b34e528eb0573dbc4cfd6fb0
Shreya Srivastava <shreyasrivastava739@gmail.com>
2026-08-04 15:58:35 +0530

    Time: 2 ms (7.46%) | Memory: 50.5 MB (5.17%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,17 +1,11 @@
-
-
 class Solution {
     public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length - 1;
-
-        while (i < j) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-
-            i++;
-            j--;
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char ch : s){
+            stack.push(ch);
+        }
+        for(int i=0; i<s.length; i++){
+            s[i]=stack.pop();
         }
     }
 }
