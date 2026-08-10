e55450bf31800ef35e80c9e6f07a71bd564fcf43
Shreya Srivastava <shreyasrivastava739@gmail.com>
2026-08-10 23:55:04 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,18 @@
+// Last updated: 10/08/2026, 23:55:05
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> freq=new HashMap<>();
+        // Count the Magazine characters
+        for(char ch : magazine.toCharArray()){
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+        // Consume charcters for ransom note
+        for(char ch : ransomNote.toCharArray()){
+            if(!freq.containsKey(ch) || freq.get(ch)==0){
+                return false;
+            }
+            freq.put(ch,freq.get(ch)-1);
+        } 
+        return true;
+    }
+}
